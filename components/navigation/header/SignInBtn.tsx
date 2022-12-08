938920da5bbe169f4f84879eaad4bd0db760eb8d
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-12-08 17:32:49 -0500

    style: sign in btn styles

--- components/navigation/header/SignInBtn.tsx
+++ components/navigation/header/SignInBtn.tsx
@@ -1,12 +1,25 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const SignInBtn = () => {
+  const [isBtnStyled, setIsBtnStyled] = useState(false);
+  const pathname = usePathname();
+
   return (
     <div className="w-[10rem] flex items-center justify-center">
       <Link
         href="/signin"
-        className="border-2 border-slate-400 hover:border-slate-500
-         px-8 py-2 rounded-md text-slate-500 font-semibold transition-all duration-200 ease-out"
+        className={`border-2 border-slate-400 px-8 py-2 rounded-md
+        text-slate-500 font-semibold transition-all duration-200 ease-out
+         ${
+           isBtnStyled && pathname !== "/signin" ? "border-slate-500" : ""
+         } transition-all duration-300 ease-out`}
+        onMouseEnter={() => setIsBtnStyled(true)}
+        onMouseLeave={() => setIsBtnStyled(false)}
+        onClick={() => setIsBtnStyled(false)}
       >
         Sign in
       </Link>
