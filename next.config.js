79e4b194490dc82f80f286425b46a1ac770b290e
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-12-08 16:28:26 -0500

    ci: next latest update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ["links.papareact.com"],
-  },
   experimental: {
     appDir: true,
+    esmExternals: false,
   },
 };
