3f686c6aac3b82daee2b868338b6509e1ee13744
Christian Fugarino <christian.fugarino.123@gmail.com>
2023-01-19 07:55:18 -0500

    style: signin/up form styles + illustration

--- app/(pages)/(auth)/signin/page.tsx
+++ app/(pages)/(auth)/signin/page.tsx
@@ -1,18 +1,35 @@
 import SignInForm from "../../../components/auth/forms/SignInForm";
 import ProtectedRoute from "../../../components/auth/routes/ProtectedRoute";
-import FormCard from "../../../components/cards/FormCard";
 
 const SignInPage = () => {
   return (
     <ProtectedRoute>
-      <main
-        className="h-[calc(100vh-113px)] sm:h-[calc(100vh-56px)]
-        py-4 px-6 overflow-y-scroll"
-      >
-        <FormCard>
-          <SignInForm />
-        </FormCard>
-      </main>
+      <section className="px-12 max-w-[1400px] mx-auto w-screen">
+        <header className="mt-1 ml-6">
+          <h1 className="font-bold text-[1.5rem]">Sign in</h1>
+          <h2 className="relative -top-2 text-[#67676d]">
+            to access all features
+          </h2>
+        </header>
+        <div className="h-[75vh] min-h-[450px] max-h-[900px]">
+          <section className="h-full">
+            <div className="flex h-full">
+              <aside className="h-full lg:w-[50%] p-2 hidden lg:flex items-center pointer-events-none">
+                <picture>
+                  <img src="/Frame1.svg" alt="rocket" className="w-[90%]" />
+                </picture>
+              </aside>
+              <article className="w-full h-full lg:w-[50%]">
+                <div className="bg-white rounded-lg shadow-md h-full w-full flex flex-col justify-center items-center">
+                  <div className="w-[65%]">
+                    <SignInForm />
+                  </div>
+                </div>
+              </article>
+            </div>
+          </section>
+        </div>
+      </section>
     </ProtectedRoute>
   );
 };
--- app/components/cards/FormCard.tsx
+++ app/components/cards/FormCard.tsx
@@ -1,25 +1,20 @@
-import Image from "next/image";
-
 interface ILargeCardProps {
   children: React.ReactNode;
 }
 
 const FormCard = ({ children }: ILargeCardProps) => {
   return (
-    <section className="bg-white p-4 rounded-lg shadow-md h-full max-w-[1400px] max-h-[1000px] min-h-[515px] mx-auto">
-      <div className="flex h-full max-h-[1000px]">
-        <aside className="relative lg:w-1/2 p-2 hidden lg:flex items-center justify-center pointer-events-none">
-          <div className="absolute">
-            <Image
-              src="/RocketB.svg"
-              width={500}
-              height={500}
-              alt="rocket illustration"
-            />
+    <section className="min-h-[515px] h-full">
+      <div className="flex h-full lg:space-x-14">
+        <aside className="h-full lg:w-[50%] p-2 hidden lg:flex items-center justify-center pointer-events-none">
+          <div className="top-0 h-[60%]">
+            <picture>
+              <img src="/Frame1.svg" alt="rocket" className="h-full" />
+            </picture>
           </div>
         </aside>
-        <article className="w-full lg:w-1/2 flex flex-col justify-center items-center">
-          <div className="w-5/6 max-w-[26rem]">{children}</div>
+        <article className="w-full h-full lg:w-[50%] flex flex-col justify-center items-center">
+          {children}
         </article>
       </div>
     </section>
