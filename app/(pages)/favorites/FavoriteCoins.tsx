3f169f32e8ac3827801be0d516989db065afbd75
Christian Fugarino <christian.fugarino.123@gmail.com>
2023-02-25 11:02:45 -0500

    fix: add favorite coins types

--- app/(pages)/favorites/FavoriteCoins.tsx
+++ app/(pages)/favorites/FavoriteCoins.tsx
@@ -13,9 +13,11 @@ import Filler from "./Filler";
 export interface StoreCoin {
   id: string;
   name: string;
+  symbol: string;
   image: string;
   price_change_percentage_24h: number;
   current_price: number;
+  market_cap: number;
 }
 
 const FavoriteCoins = () => {
