bda6daf95f0d4ed7a9568f51566e14b7b8a1e2e0
Christian Fugarino <christian.fugarino.123@gmail.com>
2023-02-28 14:05:38 -0500

    style: make coininfo price bigger

--- app/(pages)/coins/[coinid]/CoinInfo.tsx
+++ app/(pages)/coins/[coinid]/CoinInfo.tsx
@@ -70,7 +70,9 @@ const CoinInfo = ({ id }: CoinInfoProps) => {
           <div className="flex justify-between">
             <div className="flex flex-col font-bold text-[3.5rem] text-[#989898] xs:text-[5rem]">
               <h3 className="flex space-x-2 md:space-x-0 lg:space-x-2">
-                <span className="text-[0.9rem] md:hidden lg:flex">Rank:</span>
+                <span className="text-[0.9rem] xs:hidden sm:flex md:hidden lg:flex">
+                  Rank:
+                </span>
                 <span className="leading-[3.1rem]">
                   {coinInfo.market_cap_rank < 10
                     ? "0" + coinInfo.market_cap_rank
@@ -81,11 +83,11 @@ const CoinInfo = ({ id }: CoinInfoProps) => {
             <div>
               <div className="flex justify-between">
                 <div></div>
-                <div className="font-bold text-[1.5rem] ml-2 xs:text-[2rem] leading-[1.8rem] xs:leading-[2rem] w-full break-words">
+                <div className="font-bold text-[2rem] ml-2 xs:text-[3rem] md:text-[2rem] lg:text-[3rem] leading-[1.8rem] xs:leading-[2rem] w-full break-words">
                   $
                   {coinInfo.market_data.current_price["usd"]
                     .toString()
-                    .slice(0, 8)}
+                    .slice(0, 7)}
                 </div>
               </div>
               <div className="flex justify-between font-medium text-sm">
@@ -95,7 +97,7 @@ const CoinInfo = ({ id }: CoinInfoProps) => {
                     coinInfo.market_data.price_change_percentage_24h < 0
                       ? "text-red-700"
                       : "text-green-700"
-                  } text-[0.9rem]`}
+                  } text-[1rem] mt-1 xs:mt-2`}
                 >
                   {coinInfo.market_data.price_change_percentage_24h > 0
                     ? "+" + coinInfo.market_data.price_change_percentage_24h
